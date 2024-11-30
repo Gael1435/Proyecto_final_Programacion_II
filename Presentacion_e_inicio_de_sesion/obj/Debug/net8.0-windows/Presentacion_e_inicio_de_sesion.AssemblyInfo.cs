814469aea949358be94aed5445f5a0f25c69d2c8
Azael <azfajardo69@gmail.com>
2024-11-29 19:20:34 -0600

    Guardando cambios locales antes de hacer el merge

--- Presentacion_e_inicio_de_sesion/obj/Debug/net8.0-windows/Presentacion_e_inicio_de_sesion.AssemblyInfo.cs
+++ Presentacion_e_inicio_de_sesion/obj/Debug/net8.0-windows/Presentacion_e_inicio_de_sesion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentacion_e_inicio_de_sesion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b2eec01754e567dd65fdc5770399d04fe429d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentacion_e_inicio_de_sesion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentacion_e_inicio_de_sesion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

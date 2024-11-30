1fe86e7598031a3f99e52b68843c2ffd3a6a5878
Gael <elirivera_2782@hotmail.com>
2024-11-29 18:57:37 -0600

    Base de Datos

--- proyecto_final/proyecto_final/obj/Debug/net8.0-windows/proyecto_final.AssemblyInfo.cs
+++ proyecto_final/proyecto_final/obj/Debug/net8.0-windows/proyecto_final.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto_final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c31c2e5cdb56567bf80084a31e966f7ae976d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+213cfd0b9ca83d63a8a79e6e0a1b2a663d324946")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto_final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto_final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
